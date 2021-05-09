c40c9019c5c93a091cd846c0d22f4528315846fb
jujus <jujustor@gmail.com>
2021-05-09 17:57:39 +0200

    environnement begin

--- src/Modèle/Environnement.java
+++ src/Modèle/Environnement.java
@@ -1,4 +1,36 @@
 package Modèle;
 
+import java.util.ArrayList;
+
 public class Environnement {
+    private int width,height;
+    private ArrayList<Personnage> lesPerso;
+
+    public Environnement(int width,int height){
+        super();
+        this.width=width;
+        this.height=height;
+        lesPerso=new ArrayList<>();
+    }
+    public int getWidth(){
+        return width;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public ArrayList<Personnage>getPerso(){
+        return lesPerso;
+    }
+    public Personnage getPersonnage(int id){
+        for(Personnage p:lesPerso){
+            return p;
+        }
+        return null;
+    }
+    public void addPerso(Personnage p){
+        lesPerso.add(p);
+    }
+    public void deletePerso(Personnage p){
+        lesPerso.remove(p);
+    }
 }
