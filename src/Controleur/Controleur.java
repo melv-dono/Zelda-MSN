6c3bae21bef2bf428d479b6159fbe116f4362d61
jujus <jujustor@gmail.com>
2021-05-09 16:25:02 +0200

    implement JavaFx

---
+++ src/Controleur/Controleur.java
@@ -0,0 +1,13 @@
+package Controleur;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controleur implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
