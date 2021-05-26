1e52f587ba86e7dd9deff31890ab908258774e92
jujus <jujustor@gmail.com>
2021-05-26 10:32:40 +0200

    Inventaire & objet

--- src/Modèle/Inventaire.java
+++ src/Modèle/Inventaire.java
@@ -3,5 +3,38 @@ package Modèle;
 import java.util.ArrayList;
 
 public class Inventaire {
-    private ArrayList lesObjets;
+    private Objet[][] listeObjets;
+
+    public Inventaire(){
+        listeObjets=new Objet[4][4];
+    }
+
+    public void addObjet(Objet obj){
+        if(restePlaceInventaire()==false){
+            System.out.println("L'inventaire ne peut plus stocker d'objet");
+        }else{
+            for(int ligne=0;ligne<4;ligne++){
+                for(int colonne=0;colonne<4;colonne++){
+                    if(!(listeObjets[ligne][colonne] instanceof Objet)) {
+                        listeObjets[ligne][colonne] = obj;
+                    }
+                }
+            }
+        }
+
+    }
+
+    private boolean restePlaceInventaire(){
+        int placeUse=0;
+        for(int ligne=0;ligne<4;ligne++){
+            for(int colonne=0;colonne<4;colonne++){
+                if(listeObjets[ligne][colonne] instanceof Objet){
+                    placeUse++;
+                }else{
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/Modèle/Objet.java
+++ src/Modèle/Objet.java
@@ -1,9 +1,17 @@
 package Modèle;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 
 public class Objet {
     private IntegerProperty positionLargeur;
     private IntegerProperty positionHauteur;
+    private int pointsRegeneration;
+
+    public Objet(int posLargeur,int posHauteur,int ptRegen){
+        positionLargeur=new SimpleIntegerProperty(posLargeur);
+        positionHauteur=new SimpleIntegerProperty(posHauteur);
+        pointsRegeneration=ptRegen;
+    }
 
 }
